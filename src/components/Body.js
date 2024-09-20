c1a74a98670a10d311d27cea132b265be67907f4
Manish <76098501+manishkunt@users.noreply.github.com>
2024-09-21 03:38:16 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,15 +10,13 @@ const Body = () => {
 
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-  console.log("Body rendered");
-
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.95250&lng=75.71050&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.95250&lng=75.71050&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
