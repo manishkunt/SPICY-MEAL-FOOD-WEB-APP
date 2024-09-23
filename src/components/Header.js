50d5ec68d375b3823cfc6b6d6911df0a05d1816f
Manish <76098501+manishkunt@users.noreply.github.com>
2024-09-23 22:36:16 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
-
-  console.log("Header rendered");
-
-  useEffect(() => {
-    console.log("useEffect called");
-  }, [btnNameReact]);
 
   return (
     <div className="header">
@@ -17,9 +11,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="Contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login"
