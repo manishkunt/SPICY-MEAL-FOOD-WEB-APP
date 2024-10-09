35e876c641b772f9dacaf4bfc3324a5b15332e49
Manish <76098501+manishkunt@users.noreply.github.com>
2024-10-10 01:56:32 +0530

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,15 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact Us</h1>
+    <div className=" dark:bg-black min-h-screen">
+    <div className="w-11/12 m-auto">
+      <div className="border-b-[2px] border-slate-300 dark:border-gray-700">
+        <h1 className=" py-10 text-5xl dark:text-white font-semibold">Contact Us</h1>
+      </div>
+      <div className="pt-8">
+        <h1>Email: manishkuntsj@gmail.com</h1>
+        <h2>GitHub: </h2>
+      </div>
+    </div>
     </div>
   );
 };
