a863aa113a8eca91400699a5e3838471ad134e74
Manish <76098501+manishkunt@users.noreply.github.com>
2024-10-11 22:32:15 +0530

    commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,10 +5,10 @@ export const LOGO_URL =
   "https://png.pngtree.com/png-vector/20231001/ourmid/pngtree-simple-burger-logo-vector-png-image_10190021.png";
 
 export const MENU_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8942501&lng=75.8285916&restaurantId=";
+  "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8942501&lng=75.8285916&restaurantId=";
 
 export const RES_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8942501&lng=75.8285916&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8942501&lng=75.8285916&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const NO_IMG =
   "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
